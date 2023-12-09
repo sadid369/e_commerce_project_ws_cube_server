41a505fb485c3ccf550c1cdbec687d41c0dbc3f3
sadid369 <sadid.jones@gmail.com>
2023-12-09 23:33:19 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+// IMPORTS FROM PACKAGES
+const express = require("express");
+const mongoose = require("mongoose");
+const adminRouter = require("./routes/admin");
+// IMPORTS FROM OTHER FILES
+const authRouter = require("./routes/auth");
+const productRouter = require("./routes/product");
+const userRouter = require("./routes/user");
+
+// INIT
+const PORT = 3000;
+const app = express();
+const DB =
+  "mongodb+srv://sadid:123457abc@cluster0.wtek8ov.mongodb.net/e-commerce-project-wscube?retryWrites=true&w=majority";
+
+// middleware
+app.use(express.json());
+app.use(authRouter);
+app.use(adminRouter);
+app.use(productRouter);
+app.use(userRouter);
+
+// Connections
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`connected at port ${PORT}`);
+});
